db01be316fea289412f9260b352547f9bf76a35e
Danaragil <43490991+Danaragil@users.noreply.github.com>
2018-11-26 18:30:02 +0700

    Update PersegiPanjang.java

--- src/Program/PersegiPanjang.java
+++ src/Program/PersegiPanjang.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Program;
 
 public class PersegiPanjang implements BangunDatar{
@@ -12,17 +7,18 @@ public class PersegiPanjang implements BangunDatar{
     public PersegiPanjang(float panjang, float lebar) {
         this.panjang = panjang;
         this.lebar = lebar;
-//    }
-//
-//    
-//    
-//    public float luas() {
-//        return this.panjang*this.lebar;
-//    }
-//
-//    @Override
-//    public float keliling() {
-//        return this.panjang*this.lebar*2;
-//    }
+    }
+
+    
+    @Override
+    public double luas() {
+        return this.panjang*this.lebar;
+    }
+
+    @Override
+    public double keliling() {
+        return this.panjang*this.lebar*2;
+    }
     
 }
+
